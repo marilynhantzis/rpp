455d43bdc20989accf8d8edd426ab82cab9289c2
marilynhantzis <ezdramiller@gmail.com>
2024-11-30 02:03:02 +0700

    две пустые строки

--- discriminant.py
+++ discriminant.py
@@ -1,11 +1,13 @@
 def calculate_discriminant(a, b, c):
     return b**2 - 4*a*c
 
+
 class QuadraticEquation:
     def __init__(self, a, b, c):
         self.a = a
         self.b = b
         self.c = c
+
 
     def discriminant(self):
         return calculate_discriminant(self.a, self.b, self.c)
