f15c4be7dac7297fcf56c84dbce54fd9fe6a69ec
marilynhantzis <ezdramiller@gmail.com>
2024-11-29 23:16:09 +0700

    очередное исправление

--- discriminant.py
+++ discriminant.py
@@ -1,8 +1,10 @@
 def calculate_discriminant(a, b, c):  # Функция для расчета дискриминанта
     return b**2 - 4*a*c  
 
-a = float(input("Введите коэффициент a: "))
-b = float(input("Введите коэффициент b: "))  
-c = float(input("Введите коэффициент c: "))  
-discriminant = calculate_discriminant(a, b, c)  
-print(f"Дискриминант: {discriminant}")  
+if __name__ == "__main__":  # Проверяем, является ли скрипт основным
+    a = float(input("Введите коэффициент a: "))
+    b = float(input("Введите коэффициент b: "))  
+    c = float(input("Введите коэффициент c: "))  
+    discriminant = calculate_discriminant(a, b, c)  
+    print(f"Дискриминант: {discriminant}")  
+
