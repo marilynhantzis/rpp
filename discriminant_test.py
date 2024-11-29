1d25d00860cae52926f39bdd11973c645f9486b8
marilynhantzis <ezdramiller@gmail.com>
2024-11-30 02:33:26 +0700

    ошибка в тесте

--- discriminant_test.py
+++ discriminant_test.py
@@ -9,4 +9,4 @@ class TestQuadraticEquation(unittest.TestCase):
 
     def test_discriminant_positive(self):
         eq = QuadraticEquation(1, 5, 6)
-        self.assertEqual(eq.discriminant(), 1)
+        self.assertEqual(eq.discriminant(), 0)
